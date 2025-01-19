6c7a105f5c799c72c17b4bb9033df05f8ebfb303
AditiSurve <122209521+AditiSurve17@users.noreply.github.com>
2025-01-19 14:47:21 +0530

    QR for donation added

--- app/pasaydan/com/page.tsx
+++ app/pasaydan/com/page.tsx
@@ -4,6 +4,7 @@ import Vision from "@/components/homepage/vison";
 import Metrics from "@/components/homepage/metric";
 import Mission from "@/components/homepage/mission";
 import ContactUs from "@/components/homepage/contact";
+import QRSection from "@/components/homepage/QRSection";
 import Image from "next/image";
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       <TitleInfo />
       <div className="bg-gradient-to-b from-white via-[#f8faff] to-[#edf2ff] w-full flex flex-col justify-center items-center">
         <Vision />
+        <QRSection />
         <Metrics />
         <Mission />
       </div>
