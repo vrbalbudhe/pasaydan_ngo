82113477e702bf782381af00c88296a529f6e0b4
CynicPoet <128451600+CynicPoet@users.noreply.github.com>
2025-02-02 18:55:03 +0530

    updated next.config.ts for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,8 @@
 module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if there are lint errors.
+    ignoreDuringBuilds: true,
+  },
   async redirects() {
     return [
       {
