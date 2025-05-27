e9f1325815c295e2fe6071ea6202e014dc9b992b
CynicPoet <shivam.pokharkar18@gmail.com>
2025-05-27 15:47:33 +0530

    Trail changes - 1

--- components/drive/addParticipants.tsx
+++ components/drive/addParticipants.tsx
@@ -16,7 +16,7 @@ export default function AddParticipants() {
         className="rounded-none w-fit px-2 py-2 bg-blue-500 md:hover:scale-95 transition-all duration-300 cursor-pointer"
       >
         <p className="font-semibold text-md rounded-sm text-white tracking-tight">
-          Add Participants
+          Add Beneficiary
         </p>
       </div>
       {openForm && (
